caebbac33855be4cbf60c84a71ad16732ca9939e
Sander Vispoel <sandervispoel@hotmail.com>
2015-12-09 20:23:27 +0100

    42
    confirmation:
    - simple design added

--- confirmation.php
+++ confirmation.php
@@ -1,15 +1,43 @@
 <?php
+session_start();
+
 ?>
 <!DOCTYPE HTML>
-<html lang="en">
+<html lang="en" xmlns="http://www.w3.org/1999/html">
 <head>
-  <meta charset="UTF-8">
-  
+    <meta charset="UTF-8">
     <title>Classic Barbershop Jeroen</title>
-    
     <link rel="stylesheet" href="style/style.css">
+    <link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Nunito' rel='stylesheet' type='text/css'>
 </head>
 <body>
-    
+
+<header>
+    <div id="main-header">
+        <a href="index.php"><img src="http://barbershopbarcelona.com/wp-content/uploads/2014/11/thebarbershop-Redondo.png" id="header-logo"></a>
+        <span id="header-name">Classic Barbershop Jeroen</span>
+    </div>
+    <nav id="navigation-background">
+        <div class="navigation-helper">
+            <ul>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Reserveer</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
+
+<section id="main-page">
+    <img id="img-chair" src="images/confirmation/chair.png">
+    <p id="header-text-header-big">Bedankt voor het reserveren!</p>
+    <p>Tot dan!</p>
+</section>
+
+<footer>
+</footer>
+
+<script src="scripts/select.js"></script>
 </body>
 </html>
